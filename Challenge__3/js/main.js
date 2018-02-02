7808d02eaedd5c46f01e9f4eead83e6d2aba587c
Paweł <pawelczubak@op.pl>
2018-02-02 12:20:32 +0000

    Challenge__3 v0.10
    The scripts in js for nav have been finished

--- Challenge__3/js/main.js
+++ Challenge__3/js/main.js
@@ -1,10 +1,54 @@
-$(".nav__toggle").click(function () {
-  $(".nav__navbar").slideToggle();
-  $(this).toggleClass("active");
-});
+$(document).ready(function () {
 
-if ($(".nav__toggle").css('display') == 'block') {
-  $(".nav__navbar a").click(function () {
+  /*  -----  NAV TOGGLE  -----  */
+
+
+
+  $(".nav__toggle").click(function () {
     $(".nav__navbar").slideToggle();
+    $(this).toggleClass("active");
   });
-}
+
+
+  if ($(window).width() <= 768) {
+    $('nav a').click(function () {
+      $(".nav__navbar").slideToggle();
+      $(".nav__toggle").toggleClass("active");
+    });
+  }
+
+
+
+
+  /*  -----  FIXED NAV  -----  */
+
+
+
+  $(window).scroll(function () {
+    if ($(document).scrollTop() > 300) {
+      $('#nav').addClass('nav--fixed');
+    } else {
+      $('#nav').removeClass('nav--fixed');
+    }
+  });
+
+
+
+  /*  ----- SCROLL NAV -----  */
+
+
+
+  $('nav a').click(function (event) {
+    var id = $(this).attr("href");
+    var offset = 0;
+    var target = $(id).offset().top;
+
+    $('html, body').animate({
+      scrollTop: target
+    }, 500);
+    event.preventDefault();
+  });
+
+
+
+});
