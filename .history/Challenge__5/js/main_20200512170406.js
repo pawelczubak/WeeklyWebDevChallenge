6039eef7a1cbf1a59462f1dd072e858facf62183
Pawel <pawelczubak@op.pl>
2020-05-12 17:34:42 +0100

    Parallax effect is added

---
+++ .history/Challenge__5/js/main_20200512170406.js
@@ -0,0 +1,107 @@
+// PORTFOLIO GALLERY START
+
+let galleryImages = document.querySelectorAll(".portfolio__project");
+let getLatestOpenImg;
+let windowWidth = window.innerWidth;
+
+
+if (galleryImages) {
+    galleryImages.forEach(function (image, index) {
+        image.onclick = function () {
+            let getElementCss = window.getComputedStyle(image);
+            let getFullImgUrl = getElementCss.getPropertyValue("background-image");
+            let getImgUrlPos = getFullImgUrl.split("/img/");
+            let setNewImgUrl = getImgUrlPos[1].replace('")', '');
+
+            getLatestOpenImg = index + 1;
+
+            let container = document.body;
+            let newImgWindow = document.createElement("div");
+            container.appendChild(newImgWindow);
+            newImgWindow.setAttribute("class", "img-window");
+            newImgWindow.setAttribute("onclick", "closeImg()");
+
+            let newImg = document.createElement("img");
+            newImgWindow.appendChild(newImg);
+            newImg.setAttribute("src", "img/" + setNewImgUrl);
+            newImg.setAttribute("id", "current-img");
+        }
+    });
+}
+
+function closeImg() {
+    document.querySelector(".img-window").remove();
+}
+
+
+
+
+// GOOGLE MAPS
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    var uluru = {
+        lat: 50.208415,
+        lng: -5.490893
+
+    };
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 15,
+            center: uluru
+        });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+        icon: 'https://img.icons8.com/ios-filled/50/000000/marker.png'
+    });
+}
+// navigation
+
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav__list');
+    const navLinks = document.querySelectorAll('.nav__link')
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav__list--active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navListFade 0.5s ease forwards ${index / 7}s`;
+            }
+
+        });
+
+        // burgrt animate
+        burger.classList.toggle('toggle');
+    });
+
+
+
+}
+
+navSlide();
+
+
+// scroll back to top
+
+lettimeOut;
+
+function scrollToTop() {
+    if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0) {
+        window.scrollBy(0, -50);
+        timeOut = setTimeout('scrollToTop()', 5);
+    } else clearTimeout(timeOut);
+}
+
+$('.items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+});
